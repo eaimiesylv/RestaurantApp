51061b19d65b13f4db41919ec1f13a4f659da2eb
Emmanuel <Emmanuel.okum@idea2solutions.com>
2023-10-25 03:27:04 +0100

    Moved authentication logic to service class

--- api-core/app/Services/AuthService/AuthRepository.php
+++ api-core/app/Services/AuthService/AuthRepository.php
@@ -3,36 +3,24 @@
 namespace App\Services\AuthService;
 use App\Services\AuthService\AuthRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+
 use App\Models\User;
 
 class AuthRepository
 {
    
    
-    public function authenticateUser($request)
+    public function findUser($request)
     {  
             
-              $user = User::where('email', $request['email'])->first();
-              if(!$user){
-                return response()->json(['errors' => 'User not found'], 401);
-              }
+              return User::where('email', $request['email'])->first();
+             
               
-              if ($user->status === 'inactive') {
-                return response()->json(['errors' => 'Your account is inactive'], 403);
-              }
-              if(!Hash::check($request['password'],$user->password)){
-                   
-                 return response()->json(['errors' => 'Wrong credentials'], 403);
-              } 
-              $data= [
-                'token'=>$user->createToken('api-token')->plainTextToken,
-                'data' => $user
-              ];
-              return response()->json(['success' => $data], 401);
+              
 
     }
+    
+
    
       
 }
--- api-core/app/Services/AuthService/AuthService.php
+++ api-core/app/Services/AuthService/AuthService.php
@@ -2,7 +2,7 @@
 
 namespace App\Services\AuthService;
 use App\Services\AuthService\AuthRepository;
-
+use Illuminate\Support\Facades\Hash;
 
 
 class AuthService
@@ -16,10 +16,25 @@ class AuthService
 
     }
   
-    public function authenticateUser(array $user)
+    public function authenticateUser(array $request)
     {
     
-        return $this->authRepository->authenticateUser($user);
+        $user=$this->authRepository->findUser($request);
+      
+        if ($user['status'] === 'inactive') {
+            return response()->json(['errors' => 'Your account is inactive'], 403);
+        }
+
+        if(!Hash::check($request['password'],$user['password'])){
+               
+             return response()->json(['errors' => 'Wrong credentials'], 403);
+        } 
+        
+        $data= [
+            'token'=>$user->createToken('api-token')->plainTextToken,
+            'data' => $user
+          ];
+          return response()->json(['success' => $data], 200);
 
     }
     
